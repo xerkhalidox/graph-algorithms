56752a0cf21a1ba2100152f3679755d152bb6255
Khalid <khalidlodamohamed991998@gmail.com>
2020-12-10 00:29:59 +0200

    made the graph generic and accept wieghts

--- c++.cpp
+++ c++.cpp
@@ -1,60 +1,75 @@
 #include <iostream>
 #include <vector>
-#include <queue>
+#include <utility>
 
 using namespace std;
 
+template <typename T>
 class Graph{
     public:
-    vector<vector<int>> vertices;
-    int numberOfNodes;
-    Graph(int _numberOfNodes):vertices(_numberOfNodes+1, vector<int>(0)), numberOfNodes(_numberOfNodes+1) {}
+        vector<vector<pair<int, T>>> vertices;
+        int numberOfNodes{0};
+        bool cycleDetected{false};
 
-    void addEdge(int vertex, int data){
-        vertices.at(vertex).push_back(data);
-    }
+        Graph(int _numberOfNodes):vertices(_numberOfNodes+1, vector<pair<int, T>>(0)), numberOfNodes(_numberOfNodes+1) {}
 
-    void printVertices(){
-        int i=0;
-        for(auto &vertex: vertices){
-            cout<<i;
-            for(auto &edge:vertex){
-                cout<<"->"<<edge;
-            }
-            cout<<endl;
-            i++;
+        void addEdge(int vertex, int adj, T data){
+            vertices.at(vertex).push_back(make_pair(adj, data));
         }
-    }
 
-    int size(){
-        return numberOfNodes-1;
-    }
-    
-    void dfs(int node){
-        vector<int> visited(numberOfNodes,0);
-        discover(node, visited);
-    }
-    void discover(int node, vector<int> &visited){
-        visited[node]=1;
-        cout<<node<<endl;
-        for(auto &item:vertices[node]){
-            if(!visited[item]){
-                discover(item, visited);
+        void printVertices(){
+            int i=0;
+            for(auto &vertex: vertices){
+                cout<<i;
+                for(auto &edge:vertex){
+                    cout<<"->"<<edge.first<<" & "<<edge.second;
+                }
+                cout<<endl;
+                i++;
             }
         }
-    }
+
+        int size(){
+            return numberOfNodes-1;
+        }
+        
+        void dfs(int node){
+            vector<int> visited(numberOfNodes+1,0);
+            discover(node, visited);
+           for(int i=0;i<numberOfNodes;i++){
+               if(!visited[i]){
+                   discover(i, visited);
+               }
+           }
+        }
+
+        void discover(int node, vector<int> &visited){
+            visited[node]=1;
+            cout<<node<<endl;
+            for(auto &item:vertices[node]){
+                if(item.first==node){
+                    cycleDetected=true;
+                }
+                if(!visited[item.first]){
+                    discover(item.first, visited);
+                }
+            }
+        }
+
+        bool detectCycles(int node){
+           dfs(node);
+           return cycleDetected?true:false;
+        }
 };
 
 int main()
 {
-    Graph *graph=new Graph(4);
-    graph->addEdge(1, 2);
-    graph->addEdge(0, 2);
-    graph->addEdge(0, 1);
-    graph->addEdge(2, 0);
-    graph->addEdge(2, 3);
-    graph->addEdge(3, 3);
-    graph->dfs(2);
+    Graph<int> *graph=new Graph<int>(2);
+    graph->addEdge(0, 1, 5);
+    graph->addEdge(1, 0, 2);
+    graph->addEdge(2, 2, 4);
+     bool cycle=graph->detectCycles(2);
+     cout<<cycle<<endl;
     return 0;
 }
 
